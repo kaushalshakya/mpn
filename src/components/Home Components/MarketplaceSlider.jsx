8e11aa930700187ffda001bc580cb7de09dd8e98
kaushalshakya <kshakya101@gmail.com>
2023-10-12 11:30:26 +0545

    auto-scroll

--- src/components/Home Components/MarketplaceSlider.jsx
+++ src/components/Home Components/MarketplaceSlider.jsx
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Loader from "../Loader";
 
 const MarketplaceSlider = ({ queryKey, endpoint }) => {
+  const carouselRef = useRef(null);
+
   const { isLoading, error, data } = useQuery({
     queryKey: queryKey,
     queryFn: async () => {
@@ -14,6 +16,26 @@ const MarketplaceSlider = ({ queryKey, endpoint }) => {
     },
   });
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (carouselRef.current) {
+        const { scrollWidth, scrollLeft } = carouselRef.current;
+        const singleCardWidth =
+          carouselRef.current.children[0].offsetWidth + 32;
+
+        // If we're close to the end, reset scroll to start.
+        if (scrollLeft + singleCardWidth >= scrollWidth) {
+          carouselRef.current.scrollLeft = 0;
+        } else {
+          carouselRef.current.scrollLeft += singleCardWidth;
+        }
+      }
+    }, 3000); // Every 5 seconds
+
+    // Cleanup the interval on component unmount.
+    return () => clearInterval(interval);
+  }, []);
+
   if (isLoading) {
     return <Loader />;
   }
@@ -23,7 +45,10 @@ const MarketplaceSlider = ({ queryKey, endpoint }) => {
   }
 
   return (
-    <div className="carousel carousel-center lg:w-[86rem] rounded-lg space-x-4">
+    <div
+      ref={carouselRef}
+      className="carousel carousel-center lg:w-[86rem] rounded-lg space-x-4"
+    >
       {data.map((content) => (
         <div
           className="carousel-item flex bg-base-200 rounded-md cursor-pointer px-4 py-2 flex-col"
